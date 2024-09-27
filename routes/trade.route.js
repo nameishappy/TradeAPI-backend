628fbe5210c826e84f3fccc5afe877880298ed0e
nameishappy <105534032+nameishappy@users.noreply.github.com>
2024-09-27 11:07:11 +0530

    redeploy

--- routes/trade.route.js
+++ routes/trade.route.js
@@ -8,6 +8,11 @@ import {
   handleFileUpload,
 } from "../controllers/trade.controller.js";
 
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const UPLOADS_DIR = path.join(__dirname, "../uploads");
 if (!fs.existsSync(UPLOADS_DIR)) {
   fs.mkdirSync(UPLOADS_DIR);
